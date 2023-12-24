43363563f466fd9f75a81af8247f8c7a4f0f3941
Tyzen <85425438+TyzenNPJP@users.noreply.github.com>
2023-12-24 11:28:00 +0545

    W3R_1

---
+++ W3R_1_21/W3R_1_21/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program{
+    public bool twentyfunc(int a, int b)
+    {
+        int c = a + b;
+        if (a == 20 || c == 20)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void Main(String[] args)
+    {
+        Program program = new Program();
+        bool ret = program.twentyfunc(20, 15);
+        Console.WriteLine(ret);
+        Console.ReadLine();
+    }
+}
